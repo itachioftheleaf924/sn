1f27a38fbae0d88a81c3af54e38ca801cf0f6403
itachioftheleaf924 <105141523+itachioftheleaf924@users.noreply.github.com>
2022-05-21 12:57:51 -0400

    Add files via upload

---
+++ quadratic.js
@@ -0,0 +1,15 @@
+function solve(){
+
+let a = parseInt(document.getElementById('a').value);
+let b = parseInt(document.getElementById('b').value);
+let c = parseInt(document.getElementById('c').value);
+ 
+
+document.getElementById('out').value = a/2*(2*b+(a-1)*c)
+
+document.getElementById('nu').value = b+(a-1)*c
+
+
+}
+
+//n=a a=b d=c
